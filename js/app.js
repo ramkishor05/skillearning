dde35f0665e5166ec4382d2560046be27d55a6ee
Ram Kishor <ram@omniesolutions.com>
2023-09-29 20:07:06 +0530

    code update

---
+++ js/app.js
@@ -0,0 +1,35 @@
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /* Loop through a collection of all HTML elements: */
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+      elmnt = z[i];
+      /*search for elements with a certain atrribute:*/
+      file = elmnt.getAttribute("w3-include-html");
+      if (file) {
+        /* Make an HTTP request using the attribute value as the file name: */
+        xhttp = new XMLHttpRequest();
+        xhttp.open("GET", file, true);
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4) {
+            if (this.status == 200) {
+              elmnt.innerHTML = this.responseText;
+            }
+            if (this.status == 404) {
+              elmnt.innerHTML = "Page not found.";
+            }
+            /* Remove the attribute, and call this function once more: */
+            elmnt.removeAttribute("w3-include-html");
+            includeHTML();
+          }
+        }
+        xhttp.setRequestHeader('Access-Control-Allow-Origin', '*');
+        xhttp.setRequestHeader('Access-Control-Allow-Headers', 'Content-Type');
+        xhttp.setRequestHeader("X-PINGOTHER", "pingpong");
+        xhttp.setRequestHeader("Content-Type", "*");
+        xhttp.send();
+        /* Exit the function: */
+        return;
+      }
+    }
+  }
